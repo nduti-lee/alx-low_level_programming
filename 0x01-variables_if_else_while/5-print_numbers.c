77bc4418be03d29f92a9f69f20fd7123cfde2c85
nduti-lee <lindaashlee06@gmail.com>
2022-04-08 09:11:46 -0700

    digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+*Return: ALways 0 (Success)
+*/
+int main(void)
+
+{
+int digit;
+
+for (digit = 0; digit < 10; digit++)
+{
+printf("%digit", digit);
+}
+
+printf("\n");
+
+return (0);
+
+}
