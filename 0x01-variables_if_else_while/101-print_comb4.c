bd469e7f37c4a43e40d7ad17b596202bc90f42dd
nduti-lee <lindaashlee06@gmail.com>
2022-04-08 13:41:58 -0700

    com4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,6 +18,12 @@ for (c = b + 1; c < 10; c++)
 putchar((a % 10) + '0');
 putchar((b % 10) + '0');
 putchar((c % 10) + '0');
+
+if (a == 7 && b == 8 && c == 9)
+continue;
+
+putchar(',');
+putchar(' ');
 }
 }
 }
