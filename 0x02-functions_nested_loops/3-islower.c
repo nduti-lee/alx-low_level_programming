b103ad76527ad3af7de121bfe5f0a92b03c869a5
nduti-lee <lindaashlee06@gmail.com>
2022-04-12 08:01:00 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _islower - check for lower case letter
+* @c: characrer to check the case
+* Return: Always 0 (Success)
+*/
+
+int _islower(int c)
+{
+return (c >= 97 && c <= 122);
+}
