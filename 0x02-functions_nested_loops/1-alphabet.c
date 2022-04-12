61a4e0559320c0582da9d7034ab1d31394a0a666
nduti-lee <lindaashlee06@gmail.com>
2022-04-12 03:33:09 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_alphabet - Entry point
+*
+*Return: Always o (Success)
+*/
+
+void print_alphabet(void);
+{
+char C;
+
+for (C = 'a'; C <= 'z'; C++)
+{
+_putchar(C);
+}
+
+_putchar('\n');
+
+}
