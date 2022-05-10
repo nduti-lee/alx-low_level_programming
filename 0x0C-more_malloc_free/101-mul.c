8dc84ccc9bb586a75f2632d040fd731d490660f7
nduti-lee <lindaashlee06@gmail.com>
2022-05-10 16:07:50 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 
 /**
- * _atoi_digit - convert a char to integer.
+* _atoi_digit - convert a char to integer.
  * @x: character to convert.
  * Return: integer.
  **/
 
 int _atoi_digit(char x)
 {
-	unsigned int res;
+unsigned int res;
 
-	if (x <= '9' && x >= '0')
+if (x <= '9' && x >= '0')
 		res = x - '0';
 	return (res);
 }
@@ -25,7 +25,7 @@ int _isNumber(char *argv)
 {
 	int i;
 
-	for (i = 0; argv[i]; i++)
+for (i = 0; argv[i]; i++)
 		if (argv[i] < 48 || argv[i] > 57)
 			return (1);
 	return (0);
@@ -40,22 +40,17 @@ int _isNumber(char *argv)
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *tab;
+char *tab;
 	unsigned int i;
 
 	tab = malloc(size * nmemb);
 
 	if (tab == NULL)
 		return (NULL);
-
-	for (i = 0; i < (size * nmemb); i++)
-		tab[i] = '0';
-
-	return (tab);
 }
 
 /**
- * mul_array - multiply two arrays.
+* mul_array - multiply two arrays.
  * @a1: first array.
  * @len1: length of array a1.
  * @a2:  char.
@@ -66,9 +61,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
 {
-	int mul = 0, i, k;
 
-	k = lena;
+k = lena;
 	for (i = len1 - 1; i >= 0 ; i--)
 	{
 		mul += (a1[i] - '0') * (a2 - '0') + (a3[k] - '0');
@@ -78,14 +72,14 @@ void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
 	}
 
 		while (mul != 0)
-		{
-			mul += a3[k] - '0';
+{
+mul += a3[k] - '0';
 			a3[k] = (mul % 10) + '0';
 			mul /= 10;
 			k--;
-		}
+}
 
-	return (a3);
+return (a3);
 }
 /**
  * print_array - print all digits of array.
@@ -108,7 +102,7 @@ void print_array(char *a, int nb)
 }
 
 /**
- *main - print the multiplication of 2 numbers.
+*main - print the multiplication of 2 numbers.
  *@argc: array length.
  *@argv: array.
  *Return: 0.
@@ -116,19 +110,19 @@ void print_array(char *a, int nb)
 
 int main(int argc, char *argv[])
 {
-	int i, c, len1, len2, lenres;
+int i, c, len1, len2, lenres;
 	char E[6] = {'E', 'r', 'r', 'o', 'r', '\n'};
 	char *tabres;
 
-	if (argc != 3 || _isNumber(argv[1]) == 1 || _isNumber(argv[2]) == 1)
+if (argc != 3 || _isNumber(argv[1]) == 1 || _isNumber(argv[2]) == 1)
 	{
-		for (i = 0; i < 6; i++)
+for (i = 0; i < 6; i++)
 		{
-			_putchar(E[i]);
+_putchar(E[i]);
 		}
 		exit(98);
 	}
-	for (len1 = 0; argv[1][len1]; len1++)
+for (len1 = 0; argv[1][len1]; len1++)
 	;
 	for (len2 = 0; argv[2][len2]; len2++)
 	;
@@ -147,5 +141,5 @@ int main(int argc, char *argv[])
 	print_array(tabres, lenres);
 	free(tabres);
 	exit(EXIT_SUCCESS);
-	return (0);
+return (0);
 }
