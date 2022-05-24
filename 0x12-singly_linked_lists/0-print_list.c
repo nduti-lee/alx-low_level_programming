ed343a4cdf338312b22fc5bd3d2a1085359c942d
nduti-lee <lindaashlee06@gmail.com>
2022-05-24 09:27:44 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,12 +10,12 @@ size_t nelem;
 nelem = 0;
 while (h != NULL)
 {
- if (h->str == NULL)
- printf("[%d] %s\n", 0, "(nil)");
- else
- printf("[%d] %s\n", h->len, h->str);
- h = h->next;
- nelem++;
+if (h->str == NULL)
+printf("[%d] %s\n", 0, "(nil)");
+else
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+nelem++;
 }
 return (nelem);
 }
